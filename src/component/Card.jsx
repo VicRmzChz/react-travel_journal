683a2ed44b0a847c231f71cf2936f60c5d980e82
VicRmzChz <vicentermzchz@gmail.com>
2022-05-30 13:40:50 -0500

    Card finish

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -7,9 +7,10 @@ export function Card(props) {
             <div className='card'>
                 <img src={props.item.imageUrl} alt='Japan Mounten' />
                 <div className='card--info'>
-                    <span>Japan</span>
-                    <span> View on Google Maps</span>
+                    <span className='location'><i className='fa fa-map-marker'></i>{props.item.location}</span>
+                    <span className='google-map'><a href="https://goo.gl/maps/24Kg82bvmycQEDPC7">View on Google Maps</a></span>
                     <h1>Mountent Fuji</h1>
+                    <span className='data-start-end'>{props.item.startDate} - {props.item.endDate}</span>
                     <p>{props.item.description}</p>
                 </div>
             </div>
